cc790709e3a7f94678d2cfa66a285160f94de048
Xiaohu Shu <sxh20150609@gmail.com>
2020-10-25 16:04:44 +0200

    "support for room function in socket-io, and modify the JSON format"

--- routers/messageRouter.py
+++ routers/messageRouter.py
@@ -1,45 +1,91 @@
-from flask_socketio import leave_room
+from flask_socketio import leave_room, join_room
 
 from app import *
-from flask import Flask, jsonify, session, request
+from flask import Flask, jsonify, session, request, json
+
+roomForStudents = 'studentRoom'
+roomForStudentsList = []
+roomForTeachers = 'teacherRoom'
+roomForTeachersList = []
+roomForCommon = 'commonRoom'
+roomForCommonList = []
 
 
-@socketio.on('establishConnection')
+@socketio.on('connect')
 def on_connect():
-    print("is connected")
-    socketio.emit('connected', request.sid)
+    response = str(request.sid)
+    socketio.emit('established', {"sid": response}, callback=(
+        print("received by client" + response)
+    ))
 
-@socketio.on('message')
+
+@socketio.on('join')
+def on_join(group_info):
+    global roomName
+    global roomList
+    group_type = group_info["type"]
+    user_name = group_info['userName']
+    if group_type == "Student":
+        roomName = roomForStudents
+        roomList = roomForStudentsList
+        roomForStudentsList.append(request.sid)
+    elif group_type == "Teacher":
+        roomName = roomForTeachers
+        roomList = roomForTeachersList
+        roomForTeachersList.append(request.sid)
+    else:
+        roomName = roomForCommon
+        roomList = roomForCommonList
+        roomForCommonList.append(request.sid)
+
+    join_room(roomName)
+    room_join_msg_info = {
+        'type': 'userChangeInfoMessageEvent',
+        'message': user_name + ' join room ' + roomName,
+        'groupMembers': roomList
+    }
+    socketio.emit('responseMessage', room_join_msg_info, room=roomName)
+
+
+@socketio.on('requestMessage')
 def on_message(msg):
+    global msg_content
     body = msg
     print('received json: ' + str(body) + " from:" + request.sid)
     body_type = body["type"]
+
     if body_type == 'setNameEvent':
         msg_content = {
-            'type':'userChangeInfoMessageEvent',
-            'message':body['userName'] + 'enter room'
+            'type': 'userChangeInfoMessageEvent',
+            'message': body['userName'] + ' enter room'
         }
-        socketio.emit('responseMessage',msg_content, broadcast=True)
     elif body_type == 'messageEvent':
         msg_content = {
             'type': 'messageEvent',
             'message': body['message']
         }
-        socketio.emit('responseMessage', msg_content, broadcast=True)
     else:
-        socketio.emit('responseMessage', {'case':'unDefined'}, broadcast=True)
+        msg_content = {
+            'type': 'messageEvent',
+            'message': 'unDefined'
+        }
+
+    user_sid = request.sid
+    if user_sid in roomForStudentsList:
+        socketio.emit('responseMessage', msg_content, room=roomForStudents)
+    elif user_sid in roomForTeachersList:
+        socketio.emit('responseMessage', msg_content, room=roomForTeachers)
+    else:
+        socketio.emit('responseMessage', msg_content, broadcast=True)
 
 
-@socketio.on('close')
+@socketio.on('disconnect')
 def on_leave(data):
     username = session['username']
     room = data['room']
     leave_room(room)
     msg_content = {
         'type': 'userChangeInfoMessageEvent',
-        'message':username + 'enter room'
+        'message': username + 'enter room'
     }
     socketio.emit('responseMessage', jsonify(msg_content), broadcast=True)
-
-
-
